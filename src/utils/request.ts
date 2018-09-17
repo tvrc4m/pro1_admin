c0f00702505f3d83d79bc2cb0cb1149ee8c3ba3a
tvrc4m <tvrc4m@qq.com>
2018-09-17 14:36:29 +0800

    refine axios request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,14 +1,49 @@
 
 import axios, { AxiosResponse } from 'axios'
+import qs from 'qs'
+import {Message,MessageBox} from 'element-ui'
+import router from '@/router/index'
 
 const service = axios.create({
     baseURL: process.env.BASE_URL,
-    withCredentials: true
+    // withCredentials: true
+})
+
+service.interceptors.request.use(request=>{
+    request.data.token=localStorage.getItem("token")
+    request.data=qs.stringify(request.data)
+    return request
+},error=>{
+    Promise.reject(error)
 })
 
 service.interceptors.response.use(
-    response => {
-        return response
+    (response) => {
+        console.log(response)
+        if(response.status==200){
+            if(response.data['err_no']!=0){
+                if(response.data['err_no']==1001){
+                    router.push("/user/login")
+                }else if(response.data['err_no']==1002){
+                    this.confirm("登录已失效,需要重新登录","登录提示",{
+                    }).then(()=>{
+                        router.push("/user/login")
+                    })          
+                }else{
+                    this.$message({
+                        message:response.data['err_msg'],
+                        type:"error"
+                    })
+                }
+            }else{
+                return response.data
+            }
+        }else{
+            this.$message({
+                message:"请求异常",
+                type:"error"
+            })
+        }
     },
     (error: any) => {
         console.log('fetch',error.config.url,error.config.params,'error. error:',error)
