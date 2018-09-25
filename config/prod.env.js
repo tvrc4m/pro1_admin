0beb3074c857baec734e32814005bb4ec7f2d416
tvrc4m <tvrc4m@qq.com>
2018-09-25 20:40:59 +0800

    refine  base url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_URL: '"http://wangsir.org/oaadmin/"',
-    API_URL: '"http://wangsir.org/oa/index.php"',
+    BASE_URL: '"/oa/"',
+    API_URL: '"index.php"',
 }
